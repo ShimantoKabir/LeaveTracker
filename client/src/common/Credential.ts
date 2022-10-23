c29c902885aebf9ae73df2dcffa898d4b5886e48
Shahariar Kabir <maask5534@gmail.com>
2022-10-24 00:05:54 +0600

    removed

--- client/src/common/Credential.ts
+++
@@ -1,8 +0,0 @@
-export class Credential {
-	static clientId: string = "97768e48-626f-4417-a028-2032a4f47aa4";
-	static redirectUri: string = "http://localhost:3000";
-	static scope: string[] = [
-		"User.Read"
-	];
-	static authority: string = "https://login.microsoftonline.com/4pq13c.onmicrosoft.com"
-}
--- client/src/di.ts
+++
@@ -1,7 +0,0 @@
-import { Container } from "inversify";
-import {LoginComponentModel, LoginComponentModelImpl} from "./components/login/LoginComponentModel";
-
-const container = new Container();
-container.bind<LoginComponentModel>("loginComponentModel").to(LoginComponentModelImpl);
-
-export const DiContainer = container;
